c7959fb1e89511a731845cf1b80d2e7740b87948
navodRathnayake <navodhosts@gmail.com>
2022-10-21 19:23:36 +0530

    added settings UI

--- lib/Activity/SettingsActivity.dart
+++ lib/Activity/SettingsActivity.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:setting/Custom%20Widgets/Vertical&HorizontalSpace.dart';
 
 class SettingsActivty extends StatefulWidget {
-  const SettingsActivty({super.key});
+  final ThemeData themedata;
+  bool isSelected = false;
+  SettingsActivty({super.key, required this.themedata});
 
   @override
   State<SettingsActivty> createState() => _SettingsActivtyState();
@@ -10,6 +13,59 @@ class SettingsActivty extends StatefulWidget {
 class _SettingsActivtyState extends State<SettingsActivty> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    ThemeData themedata = Theme.of(context);
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  IconButton(
+                    icon: Icon(
+                      Icons.arrow_back_ios,
+                      color: themedata.primaryColor,
+                      size: 30,
+                    ),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  Text(
+                    'Settings',
+                    style: themedata.textTheme.headline1,
+                  )
+                ],
+              ),
+              const VerticalSpace(height: 30),
+              const Divider(),
+              ListTile(
+                leading: Icon(Icons.dark_mode, color: themedata.primaryColor),
+                title: Text(
+                  'Dark Mode ON / OFF',
+                  style: themedata.textTheme.headline2,
+                ),
+                subtitle: Text(
+                  'Change Your UI Theme',
+                  style: themedata.textTheme.titleSmall,
+                ),
+                trailing: Switch(
+                  value: widget.isSelected,
+                  onChanged: (value) {
+                    setState(() {
+                      widget.isSelected = !widget.isSelected;
+                    });
+                  },
+                  activeColor: themedata.secondaryHeaderColor,
+                ),
+              ),
+              const Divider(),
+              const VerticalSpace(height: 10)
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
