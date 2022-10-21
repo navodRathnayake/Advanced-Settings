df6646af8e69f836e6511aa593d537ffdb3bca76
navodRathnayake <navodhosts@gmail.com>
2022-10-21 11:59:55 +0530

    added basic dart codes and files

---
+++ lib/Activity/HomeActivity.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeActivity extends StatefulWidget {
+  const HomeActivity({super.key});
+
+  @override
+  State<HomeActivity> createState() => _HomeActivityState();
+}
+
+class _HomeActivityState extends State<HomeActivity> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/Activity/SettingsActivity.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SettingsActivty extends StatefulWidget {
+  const SettingsActivty({super.key});
+
+  @override
+  State<SettingsActivty> createState() => _SettingsActivtyState();
+}
+
+class _SettingsActivtyState extends State<SettingsActivty> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
